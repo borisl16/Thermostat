121e0ba4db190000786b35c0e910de55dd56f8cf
Borislav Lazarov <lazarov_borislav@yahoo.com>
2021-09-29 12:07:44 +0100

    Thermostat: business logic

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,3 +1,70 @@
 'use strict';
 
-class Thermostat{};
+class Thermostat{
+  constructor() {
+    this.MINIMUM_TEMEPARTURE = 10;
+    this.powerSavingMode = true;
+    this.MAX_LIMIT_PSM_ON = 25;
+    this.MIN_LIMIT_PSM_OFF = 32;
+    this.DEFAULT_TEMPERATURE = 20;
+    this.temperature = this.DEFAULT_TEMPERATURE;
+    this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
+    this.HIGH_ENERGY_USAGE_LIMIT = 25;
+  }
+
+  isMinimumTemperature() {
+    return this.temperature === this.MINIMUM_TEMEPARTURE;
+  }
+
+  getCurrentTemperature() {
+    return this.temperature
+  }
+
+  up() { 
+    if (this.isMaximumTemperature()) {
+      return;
+    }
+    this.temperature += 1
+  }
+
+  down() {
+    if (this.isMinimumTemperature()) {
+      return;
+    }
+    this.temperature -= 1
+  }
+
+  isPowerSavingModeOn() {
+    return this.powerSavingMode === true;
+  }
+
+  switchPowerSavingModeOff() {
+    this.powerSavingMode = false;
+  } 
+
+  switchPowerSavingModeOn() {
+    this.powerSavingMode = true;
+  }
+
+  isMaximumTemperature() {
+    if (this.isPowerSavingModeOn() === false) {
+      return this.temperature === this.MIN_LIMIT_PSM_OFF;
+    }
+      return this.temperature === this.MAX_LIMIT_PSM_ON;
+  }
+
+  resetTemperature() {
+    this.temperature = this.DEFAULT_TEMPERATURE;
+  }
+
+  energyUsage() {
+    if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+      return 'low-usage';
+    }
+    if (this.temperature <= this.HIGH_ENERGY_USAGE_LIMIT) {
+      return 'medium-usage';
+    }
+    return 'high-usage';
+  }
+
+};
