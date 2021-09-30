5ab52bf5343eefd425095e7c4bac7f06e226e53d
Lilly Daniell <daniell.lilly@gmail.com>
2021-09-30 15:34:49 +0100

    DOM button functionality

--- src/interface.js
+++ src/interface.js
@@ -14,3 +14,20 @@ document.querySelector('#temperature-down').addEventListener('click', () => {
     thermostat.down(); 
     document.querySelector('#temperature').innerText = thermostat.temperature; 
 })
+
+document.querySelector('#temperature-reset').addEventListener('click', () => { 
+  thermostat.resetTemperature(); 
+  document.querySelector('#temperature').innerText = thermostat.temperature; 
+})
+
+document.querySelector('#powersaving-off').addEventListener('click', () => { 
+  thermostat.switchPowerSavingModeOff(); 
+  document.querySelector('#temperature').innerText = thermostat.temperature; 
+  document.querySelector('#power-saving-status').innerText = 'off'
+})
+
+document.querySelector('#powersaving-on').addEventListener('click', () => { 
+  thermostat.switchPowerSavingModeOn(); 
+  document.querySelector('#temperature').innerText = thermostat.temperature; 
+  document.querySelector('#power-saving-status').innerText = 'on'
+})
