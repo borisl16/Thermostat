e9b6d2313dad52b6f31d1bbaa63de0bf5ac4441d
Borislav Lazarov <lazarov_borislav@yahoo.com>
2021-09-30 17:25:15 +0100

    Working on API

--- src/interface.js
+++ src/interface.js
@@ -1,37 +1,59 @@
-document.addEventListener("DOMContentLoaded", () =>  {
+// interface.js
+document.addEventListener("DOMContentLoaded", () => {
     const updateTemperature = () => {
-    document.querySelector('#temperature').innerText = thermostat.temperature;
-    document.querySelector('#temperature').className = thermostat.energyUsage();
-  }
+      document.querySelector('#temperature').innerText = thermostat.temperature;
+      document.querySelector('#temperature').className = thermostat.energyUsage();
+    }
+      
+    const thermostat = new Thermostat();
+    updateTemperature();
+  
+    document.querySelector('#temperature-up').addEventListener('click', () => {
+      thermostat.up();
+      updateTemperature();
+    });
+  
+    document.querySelector('#temperature-down').addEventListener('click', () => {
+      thermostat.down();
+      updateTemperature();
+    });
+  
+    document.querySelector('#temperature-reset').addEventListener('click', () => {
+      thermostat.resetTemperature();
+      updateTemperature();
+    });
+  
+    document.querySelector('#powersaving-on').addEventListener('click', () => {
+      thermostat.switchPowerSavingModeOn();
+      document.querySelector('#power-saving-status').innerText = 'on';
+      updateTemperature();
+    })
+  
+    document.querySelector('#powersaving-off').addEventListener('click', () => {
+      thermostat.switchPowerSavingModeOff();
+      document.querySelector('#power-saving-status').innerText = 'off';
+      updateTemperature();
+    })
+  });
+  
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=73d6c8600df5a4db82b38b94a7c8215f&units=metric')
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    document.querySelector('#current-temperature').innerText = data.main.temp;
+  });
+ 
+  const selectElement = document.querySelector('#current-city');
+selectElement.addEventListener('change', (event) => {
+  const city = event.target.value;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=73d6c8600df5a4db82b38b94a7c8215f&units=metric`
 
-const thermostat = new Thermostat();
-updateTemperature();
-
-document.querySelector('#temperature-up').addEventListener('click', () => { 
-  thermostat.up(); 
-  updateTemperature(); 
-})
-
-document.querySelector('#temperature-down').addEventListener('click', () => { 
-  thermostat.down(); 
-  updateTemperature();
-})
-
-document.querySelector('#temperature-reset').addEventListener('click', () => { 
-  thermostat.resetTemperature(); 
-  updateTemperature(); 
-})
-
-document.querySelector('#powersaving-off').addEventListener('click', () => { 
-  thermostat.switchPowerSavingModeOff(); 
-  document.querySelector('#power-saving-status').innerText = 'off'
-  updateTemperature();
-})
-
-document.querySelector('#powersaving-on').addEventListener('click', () => { 
-  thermostat.switchPowerSavingModeOn(); 
-  document.querySelector('#power-saving-status').innerText = 'on'
-  updateTemperature();
-})
+  fetch(url)
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      document.querySelector('#current-temperature').innerText = data.main.temp;
+    })
 });
-
